32d8500b188223c728cb768d3f40a26f326e5a68
Shantanu1960 <sk1960@srmist.edu.in>
2026-08-29 11:45:37 +0530

    session 4 class problems

---
+++ step_semester_3/src/main/java/arrays/class_problems/rotate.java
@@ -0,0 +1,48 @@
+package arrays.class_problems;
+
+import java.util.Scanner;
+
+public class rotate {
+
+    static int[] rotateArray(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        int[] newArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            newArray[(i + k) % n] = nums[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        int[] result = rotateArray(nums, k);
+
+        System.out.println("Rotated array:");
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}

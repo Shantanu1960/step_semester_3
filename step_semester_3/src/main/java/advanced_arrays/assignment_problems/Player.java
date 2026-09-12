223d74b0d4ec20b8ff40f1a527ff8fa4a0ae1a3c
Shantanu1960 <sk1960@srmist.edu.in>
2026-09-12 11:26:26 +0530

    Complete advanced arrays assignment problems for Session 5

---
+++ step_semester_3/src/main/java/advanced_arrays/assignment_problems/Player.java
@@ -0,0 +1,75 @@
+package advanced_arrays.assignment_problems;
+
+import java.util.Arrays;
+
+public class Player implements Comparable<Player> {
+
+    String name;
+    int matchesPlayed;
+    double battingAverage;
+    boolean injured;
+
+    public Player(String name, int matchesPlayed, double battingAverage, boolean injured) {
+        this.name = name;
+        this.matchesPlayed = matchesPlayed;
+        this.battingAverage = battingAverage;
+        this.injured = injured;
+    }
+
+    static boolean isDraftable(int matchesPlayed) {
+        return matchesPlayed >= 10;
+    }
+
+    static boolean isDraftable(int matchesPlayed, boolean injured) {
+        return matchesPlayed >= 5 && !injured;
+    }
+
+    public int compareTo(Player other) {
+        return Double.compare(other.battingAverage, this.battingAverage);
+    }
+
+    static String draftAndRank(Player[] players) {
+
+        Player[] draftable = new Player[players.length];
+        int count = 0;
+
+        for (Player p : players) {
+
+            if (isDraftable(p.matchesPlayed) ||
+                isDraftable(p.matchesPlayed, p.injured)) {
+
+                draftable[count] = p;
+                count++;
+            }
+        }
+
+        Player[] result = Arrays.copyOf(draftable, count);
+
+        Arrays.sort(result);
+
+        String ans = "";
+
+        for (int i = 0; i < result.length; i++) {
+
+            ans += (i + 1) + ". " + result[i].name;
+
+            if (i < result.length - 1) {
+                ans += " | ";
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        Player[] players = {
+            new Player("Virat", 15, 48.0, false),
+            new Player("Rahul", 7, 55.0, false),
+            new Player("Sameer", 3, 60.0, false),
+            new Player("Dev", 12, 20.0, true)
+        };
+
+        System.out.println(draftAndRank(players));
+    }
+}
